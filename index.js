8648b21faa66650e8abd932c7f6493afe21635d6
Руслан Белый <112949833+RuslanBeliy@users.noreply.github.com>
2024-11-21 13:59:57 +0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,40 @@
+const quantity = document.getElementById('quantity');
+const priceSell = document.getElementById('price-sell');
+const priceBuy = document.getElementById('price-buy');
+const result = document.querySelector('.result');
+
+quantity.value = +localStorage.getItem('quantity') || 1;
+
+let res = 0;
+
+const setResult = () => {
+  result.textContent = `Результат: ${res.toFixed(2)}`;
+};
+
+quantity.addEventListener('input', (e) => {
+  const count = +e.target.value;
+  const priceS = +priceSell.value;
+  const priceB = +priceBuy.value;
+
+  res = (count * priceS) / priceB;
+  localStorage.setItem('quantity', res);
+  setResult();
+});
+
+priceSell.addEventListener('input', (e) => {
+  const count = +quantity.value;
+  const priceS = e.target.value ? +e.target.value : 1;
+  const priceB = priceBuy.value ? +priceBuy.value : 1;
+
+  res = (count * priceS) / priceB;
+  setResult();
+});
+
+priceBuy.addEventListener('input', (e) => {
+  const count = +quantity.value;
+  const priceS = priceSell.value ? +priceSell.value : 1;
+  const priceB = e.target.value ? +e.target.value : 1;
+
+  res = (count * priceS) / priceB;
+  setResult();
+});
